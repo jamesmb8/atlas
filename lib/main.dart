0ef38800eeae4e47fb855c16ee82edd31f321563
jamesmb8 <james.briggs1109@gmail.com>
2026-01-27 23:10:23 +0000

    started

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AtlasApp extends StatelessWidget {
+  const AtlasApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'atlas',
+
+      home: const SizedBox.shrink(),
+    );
+  }
+}
